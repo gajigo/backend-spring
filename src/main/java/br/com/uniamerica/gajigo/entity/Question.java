59a0279bf92011c6d9ca0222235a58bcade2658d
Pedro Henrique <pehdfms@gmail.com>
2022-04-11 20:41:02 -0300

    make code compilable again

--- src/main/java/br/com/uniamerica/gajigo/entity/Question.java
+++ src/main/java/br/com/uniamerica/gajigo/entity/Question.java
@@ -3,17 +3,19 @@ package br.com.uniamerica.gajigo.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import java.util.Set;
 
 @Entity
 @Table(schema = "public", name = "questions")
+@SQLDelete(sql = "UPDATE questions SET removed = CURRENT_TIMESTAMP WHERE id = ?")
+@Where(clause = "removed IS null")
 @NoArgsConstructor
 @Getter @Setter
-public class Question {
-    private String answer;
-    private User responder;
-    private Set<User> voters;
+public class Question extends AbstractEntity {
 }
--- src/main/java/br/com/uniamerica/gajigo/entity/Telephone.java
+++ src/main/java/br/com/uniamerica/gajigo/entity/Telephone.java
@@ -3,11 +3,15 @@ package br.com.uniamerica.gajigo.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 
 @Entity
 @Table(schema = "public", name = "telephones")
+@SQLDelete(sql = "UPDATE telephones SET removed = CURRENT_TIMESTAMP WHERE id = ?")
+@Where(clause = "removed IS null")
 @NoArgsConstructor
 @Getter @Setter
 public class Telephone extends AbstractEntity {
