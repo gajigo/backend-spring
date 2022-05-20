6dea82edba0b718e0c3d7d2f495c94ede1a023f9
Pedro Henrique <pehdfms@gmail.com>
2022-05-20 20:45:24 -0300

    add test profile

--- src/test/java/br/com/uniamerica/gajigo/unit/AbstractUnitTest.java
+++ src/test/java/br/com/uniamerica/gajigo/unit/AbstractUnitTest.java
@@ -2,9 +2,11 @@ package br.com.uniamerica.gajigo.unit;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Profile("test")
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 public abstract class AbstractUnitTest {
