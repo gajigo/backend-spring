bec7d4b3f9561274fed3c846ea16fec6bf28bf52
Pedro Henrique <pehdfms@gmail.com>
2022-05-21 14:10:27 -0300

    Revert "remove integration tests for now"
    This reverts commit 2c5b3c1355631786c68125e491b1f7ecdc880386.

---
+++ src/test/java/br/com/uniamerica/gajigo/integration/AbstractSingularTest.java
@@ -0,0 +1,113 @@
+package br.com.uniamerica.gajigo.integration;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.jdbc.JdbcTestUtils;
+import org.springframework.test.web.servlet.ResultActions;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public abstract class AbstractSingularTest extends AbstractTest {
+    private String resource;
+    private String path;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public AbstractSingularTest(String resource) {
+        this.resource = resource;
+        this.path = root + resource;
+    }
+
+    public void teardown(String resource) {
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, resource);
+    }
+
+    public void teardown() throws Exception {
+        teardown(resource);
+    }
+
+    ResultActions postObject(String json) throws Exception {
+        return postObject(json, path);
+    }
+
+    ResultActions putObject(String json) throws Exception {
+        return putObject(json, path);
+    }
+
+    ResultActions patchObject(String json) throws Exception {
+        return patchObject(json, path);
+    }
+
+    String simplePostAndDuplicateTest(String json) throws Exception {
+        String url = getUrl(postObject(json).andExpect(status().isCreated()));  // First object should be created
+        postObject(json).andExpect(status().isConflict()); // Duplicate shouldn't
+
+        return url;
+    }
+
+    String getUrl(ResultActions result) throws Exception {
+        String response = result.andReturn().getResponse().getContentAsString();
+        if (response == null) return null;
+
+        InputStream stream = new ByteArrayInputStream(response.getBytes());
+        JsonReader reader = Json.createReader(stream);
+
+        try {
+            String self = reader.readObject()
+                    .getJsonObject("_links")
+                    .getJsonObject("self")
+                    .getString("href");
+
+            return self;
+        } catch (Exception ignored) {}
+
+        return null;
+    }
+
+    String getUri(String url) {
+        return url.substring(url.indexOf("/api"));
+    }
+
+    public abstract String validJson(String discriminator) throws Exception;
+
+    public abstract String validJson() throws Exception;
+
+    @Test
+    public void testEmptyJson() throws Exception {
+        String json = "{}";
+        postObject(json).andExpect(status().isBadRequest()); // Empty object shouldn't post
+    }
+
+    @Test
+    public void testHappyPath() throws Exception {
+        // Resource collection should load
+        tryLoading(path);
+
+        // Object should be created and disallow duplicates
+        String url = simplePostAndDuplicateTest(validJson());
+
+        // Individual resource should load
+        tryLoading(getUri(url));
+
+        // Individual object should be deleted
+        this.mockMvc.perform(delete(url))
+                .andExpect(status().isNoContent());
+
+        // Individual object page should return 404 after deletion
+        this.mockMvc.perform(get(url))
+                .andExpect(status().isNotFound());
+
+        teardown();
+    }
+}
