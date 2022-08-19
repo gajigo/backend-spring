c6515c4369f077bd8d6d66d8cc4fd7fd2eca893b
edua2127 <magalhaeseduardo91@gmail.com>
2022-08-19 19:27:24 -0300

    refactor: codigo dos testes de usuario e evento foram melhorados

--- src/test/java/br/com/uniamerica/gajigo/integration/EventTest.java
+++ src/test/java/br/com/uniamerica/gajigo/integration/EventTest.java
@@ -1,19 +1,7 @@
 package br.com.uniamerica.gajigo.integration;
 
-import br.com.uniamerica.gajigo.entity.AttendanceMode;
-import br.com.uniamerica.gajigo.entity.Event;
-import br.com.uniamerica.gajigo.entity.EventStatus;
-import br.com.uniamerica.gajigo.entity.User;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.annotation.Bean;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import java.time.LocalDateTime;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 public class EventTest extends AbstractIntegrationTest {
     public EventTest() {
@@ -21,20 +9,18 @@ public class EventTest extends AbstractIntegrationTest {
     }
 
 
-    @Test
-    public void testInsert() throws Exception  {
-        String json = "\n" +
+    public String createUser() {
+        return "\n" +
                 "{\n" +
                 "    \"name\": \"eduardo\",\n" +
                 "    \"email\":\"eduardo@gmail.com\",\n" +
                 "    \"username\": \"eduardo.sm\",\n" +
                 "    \"password\": \"123\"\n" +
                 "}";
+    }
 
-        String caminho = "http://localhost:8080/api/users";
-        post(caminho, json).andExpect(status().is2xxSuccessful());
-
-        json = "{\n" +
+    public String createEvent() {
+        return "{\n" +
                 "    \"name\": \"eduardo\",\n" +
                 "    \"attendanceMode\": \"Online\",\n" +
                 "    \"interval\": {\n" +
@@ -43,87 +29,36 @@ public class EventTest extends AbstractIntegrationTest {
                 "    },\n" +
                 "    \"owner\": \"http://localhost:8080/api/users/1\"\n" +
                 "}";
-        post(path, json).andExpect(status().is2xxSuccessful());
+    }
+    @Test
+    public void testInsert() throws Exception  {
+
+        String caminho = "http://localhost:8080/api/users";
+        post(caminho, createUser()).andExpect(status().is2xxSuccessful());
+        post(path, createEvent()).andExpect(status().is2xxSuccessful());
     }
     @Test
     public void testFindById() throws Exception {
-        String json = "\n" +
-                "{\n" +
-                "    \"name\": \"eduardo\",\n" +
-                "    \"email\":\"eduardo@gmail.com\",\n" +
-                "    \"username\": \"eduardo.sm\",\n" +
-                "    \"password\": \"123\"\n" +
-                "}";
-
         String caminho = "http://localhost:8080/api/users";
-        post(caminho, json).andExpect(status().is2xxSuccessful());
-
-        json = "{\n" +
-                "    \"name\": \"eduardo\",\n" +
-                "    \"attendanceMode\": \"Online\",\n" +
-                "    \"interval\": {\n" +
-                "        \"startDate\": \"2023-07-15T00:00:00\",\n" +
-                "        \"endDate\": \"2023-07-25T00:00:00\"\n" +
-                "    },\n" +
-                "    \"owner\": \"http://localhost:8080/api/users/1\"\n" +
-                "}";
-        post(path, json).andExpect(status().is2xxSuccessful());
-
+        post(caminho, createUser()).andExpect(status().is2xxSuccessful());
+        post(path, createEvent()).andExpect(status().is2xxSuccessful());
         getById(path, 1L).andExpect(status().is2xxSuccessful());
     }
     @Test
     public void testFindAll() throws Exception {
-        String json = "\n" +
-                "{\n" +
-                "    \"name\": \"eduardo\",\n" +
-                "    \"email\":\"eduardo@gmail.com\",\n" +
-                "    \"username\": \"eduardo.sm\",\n" +
-                "    \"password\": \"123\"\n" +
-                "}";
-
         String caminho = "http://localhost:8080/api/users";
-        post(caminho, json).andExpect(status().is2xxSuccessful());
-
-        json = "{\n" +
-                "    \"name\": \"eduardo\",\n" +
-                "    \"attendanceMode\": \"Online\",\n" +
-                "    \"interval\": {\n" +
-                "        \"startDate\": \"2023-07-15T00:00:00\",\n" +
-                "        \"endDate\": \"2023-07-25T00:00:00\"\n" +
-                "    },\n" +
-                "    \"owner\": \"http://localhost:8080/api/users/1\"\n" +
-                "}";
-        post(path, json).andExpect(status().is2xxSuccessful());
-
+        post(caminho, createUser()).andExpect(status().is2xxSuccessful());
+        post(path, createEvent()).andExpect(status().is2xxSuccessful());
         get(path).andExpect(status().is2xxSuccessful());
     }
 
     @Test
     public void testUpdate() throws Exception {
-        String json = "\n" +
-                "{\n" +
-                "    \"name\": \"eduardo\",\n" +
-                "    \"email\":\"eduardo@gmail.com\",\n" +
-                "    \"username\": \"eduardo.sm\",\n" +
-                "    \"password\": \"123\"\n" +
-                "}";
-
         String caminho = "http://localhost:8080/api/users";
-        post(caminho, json).andExpect(status().is2xxSuccessful());
-
-        json = "{\n" +
-                "    \"name\": \"eduardo\",\n" +
-                "    \"attendanceMode\": \"Online\",\n" +
-                "    \"interval\": {\n" +
-                "        \"startDate\": \"2023-07-15T00:00:00\",\n" +
-                "        \"endDate\": \"2023-07-25T00:00:00\"\n" +
-                "    },\n" +
-                "    \"owner\": \"http://localhost:8080/api/users/1\"\n" +
-                "}";
-        post(path, json).andExpect(status().is2xxSuccessful());
-
-        json = "{\n" +
-                "    \"name\": \"Lucas\",\n" +
+        post(caminho, createUser()).andExpect(status().is2xxSuccessful());
+        post(path, createEvent()).andExpect(status().is2xxSuccessful());
+        String json = "{\n" +
+                "    \"name\": \"Novo Nome\",\n" +
                 "    \"removed\": false,\n" +
                 "    \"attendanceMode\": \"Online\",\n" +
                 "    \"interval\": {\n" +
@@ -137,58 +72,20 @@ public class EventTest extends AbstractIntegrationTest {
 
     @Test
     public void testDelete() throws Exception{
-        String json = "\n" +
-                "{\n" +
-                "    \"name\": \"eduardo\",\n" +
-                "    \"email\":\"eduardo@gmail.com\",\n" +
-                "    \"username\": \"eduardo.sm\",\n" +
-                "    \"password\": \"123\"\n" +
-                "}";
-
         String caminho = "http://localhost:8080/api/users";
-        post(caminho, json).andExpect(status().is2xxSuccessful());
-
-        json = "{\n" +
-                "    \"name\": \"eduardo\",\n" +
-                "    \"attendanceMode\": \"Online\",\n" +
-                "    \"interval\": {\n" +
-                "        \"startDate\": \"2023-07-15T00:00:00\",\n" +
-                "        \"endDate\": \"2023-07-25T00:00:00\"\n" +
-                "    },\n" +
-                "    \"owner\": \"http://localhost:8080/api/users/1\"\n" +
-                "}";
-        post(path, json).andExpect(status().is2xxSuccessful());
-
+        post(caminho, createUser()).andExpect(status().is2xxSuccessful());
+        post(path, createEvent()).andExpect(status().is2xxSuccessful());
         delete(path, 1L).andExpect(status().is2xxSuccessful());
     }
 
     @Test
     public void testDisable() throws Exception {
-        String json = "\n" +
-                "{\n" +
-                "    \"name\": \"eduardo\",\n" +
-                "    \"email\":\"eduardo@gmail.com\",\n" +
-                "    \"username\": \"eduardo.sm\",\n" +
-                "    \"password\": \"123\"\n" +
-                "}";
-
         String caminho = "http://localhost:8080/api/users";
-        post(caminho, json).andExpect(status().is2xxSuccessful());
-
-        json = "{\n" +
-                "    \"name\": \"eduardo\",\n" +
-                "    \"attendanceMode\": \"Online\",\n" +
-                "    \"interval\": {\n" +
-                "        \"startDate\": \"2023-07-15T00:00:00\",\n" +
-                "        \"endDate\": \"2023-07-25T00:00:00\"\n" +
-                "    },\n" +
-                "    \"owner\": \"http://localhost:8080/api/users/1\"\n" +
-                "}";
-        post(path, json).andExpect(status().is2xxSuccessful());
-
-        json = "{\n" +
+        post(caminho, createUser()).andExpect(status().is2xxSuccessful());
+        post(path, createEvent()).andExpect(status().is2xxSuccessful());
+        String json = "{\n" +
                 "    \"removed\": true\n" +
                 "}";
-        disable(path, 1L, json).andExpect(status().is2xxSuccessful());;
+        disable(path, 1L, json).andExpect(status().is2xxSuccessful());
     }
 }
--- src/test/java/br/com/uniamerica/gajigo/integration/UserTest.java
+++ src/test/java/br/com/uniamerica/gajigo/integration/UserTest.java
@@ -1,14 +1,8 @@
 package br.com.uniamerica.gajigo.integration;
 
-import br.com.uniamerica.gajigo.entity.AttendanceMode;
-import br.com.uniamerica.gajigo.entity.Event;
-import br.com.uniamerica.gajigo.entity.EventStatus;
-import br.com.uniamerica.gajigo.entity.User;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Test;
-import org.springframework.test.web.servlet.RequestBuilder;
 
-import java.util.List;
+import org.junit.jupiter.api.Test;
+
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -16,36 +10,53 @@ public class UserTest extends AbstractIntegrationTest {
     public UserTest() {
         super("users");
     }
+
     @Test
-    public void testDisable() throws Exception {
-        String json = "\n" +
-                "{\n" +
-                "    \"name\": \"eduardo\",\n" +
+    public void testInsert() throws Exception {
+        post(path, createdUser()).andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        post(path, createdUser()).andExpect(status().is2xxSuccessful());
+
+        String json = "{\n" +
+                "    \"name\": \"Novo Nome\",\n" +
                 "    \"email\":\"eduardo@gmail.com\",\n" +
+                "    \"removed\": false,\n" +
                 "    \"username\": \"eduardo.sm\",\n" +
                 "    \"password\": \"123\"\n" +
                 "}";
+        put(path,1L, json).andExpect(status().is2xxSuccessful());
+    }
+    @Test
+    public void testDisable() throws Exception {
+        post(path, createdUser()).andExpect(status().is2xxSuccessful());
 
-        post(path, json).andExpect(status().is2xxSuccessful());
-
-        json = "{\n" +
+        String json = "{\n" +
                 "    \"removed\": true\n" +
                 "}";
         disable(path, 1L, json).andExpect(status().is2xxSuccessful());
     }
 
-    @Test
-    public void testFindAll() throws Exception {
-        String json = "\n" +
+    public String createdUser() {
+        return "\n" +
                 "{\n" +
                 "    \"name\": \"eduardo\",\n" +
                 "    \"email\":\"eduardo@gmail.com\",\n" +
                 "    \"username\": \"eduardo.sm\",\n" +
                 "    \"password\": \"123\"\n" +
                 "}";
+    }
 
-        post(path, json).andExpect(status().is2xxSuccessful());
-
+    @Test
+    public void testFindById() throws Exception {
+        post(path, createdUser()).andExpect(status().is2xxSuccessful());
+        getById(path, 1L);
+    }
+    @Test
+    public void testFindAll() throws Exception {
+        post(path, createdUser()).andExpect(status().is2xxSuccessful());
         get(path).andExpect(status().is2xxSuccessful());
     }
 }
