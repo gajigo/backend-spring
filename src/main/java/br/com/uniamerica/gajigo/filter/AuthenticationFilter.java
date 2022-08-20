d617e68e173c1d8cb5d9b7358d94a63b7bdec0e7
Helcio L. do Carmo Junior <helcio@carmo.in>
2022-08-20 14:26:33 -0300

    fix: Change JWT token roles to use list instead of joining

--- src/main/java/br/com/uniamerica/gajigo/filter/AuthenticationFilter.java
+++ src/main/java/br/com/uniamerica/gajigo/filter/AuthenticationFilter.java
@@ -47,7 +47,7 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
                 .withSubject(user.getUsername())
                 .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
                 .withIssuer(request.getRequestURL().toString())
-                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining()))
+                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                 .sign(algorithm);
 
         String refreshToken = JWT.create()
