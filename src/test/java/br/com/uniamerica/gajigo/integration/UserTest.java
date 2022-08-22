effa7891f8ac1d7e1a04b46d4cfedbea3c88dd4c
changzer <changzer@gmail.com>
2022-08-22 11:37:02 -0300

    createAndDeleteUser() cria um usuario, dá um get para verificar se foi criado com sucesso, depois procura pelo id e deleta o usuario criado.

--- src/test/java/br/com/uniamerica/gajigo/integration/UserTest.java
+++ src/test/java/br/com/uniamerica/gajigo/integration/UserTest.java
@@ -1,19 +1,10 @@
 package br.com.uniamerica.gajigo.integration;
-
-import br.com.uniamerica.gajigo.entity.User;
-import br.com.uniamerica.gajigo.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.List;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -42,4 +33,24 @@ public class UserTest extends AbstractIntegrationTest {
         get(path).andExpect(content().string(containsString("John Doe")));
 
     }
+
+    @Test
+    public void createAndDeleteUser() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode user = mapper.createObjectNode();
+        user.put("name", "John Doe");
+        user.put("email", "john.doe@example.com");
+        user.put("username", "JohnDoe");
+        user.put("password", "john.doe");
+
+        String json = mapper.writeValueAsString(user);
+
+        post(path,json).andExpect(status().isCreated());
+        get(path).andExpect(content().string(containsString("John Doe")));
+
+        mockMvc.perform( MockMvcRequestBuilders.delete("/api/users/{id}",1))
+                .andExpect(status().is2xxSuccessful());
+
+    }
+
 }
