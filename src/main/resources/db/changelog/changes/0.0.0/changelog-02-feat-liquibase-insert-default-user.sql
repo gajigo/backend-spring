075311003e06dba8bc198a152f79458081dbe88e
Helcio L. do Carmo Junior <helcio@carmo.in>
2022-08-28 20:32:52 -0300

    fix: Add default user password

--- src/main/resources/db/changelog/changes/0.0.0/changelog-02-feat-liquibase-insert-default-user.sql
+++ src/main/resources/db/changelog/changes/0.0.0/changelog-02-feat-liquibase-insert-default-user.sql
@@ -3,4 +3,4 @@
 -- changeset helci:280820221803-1
 INSERT INTO users
     (created, updated, description, name, admin, email, password, username)
-VALUES (now(), now(), '', 'Admin', true, 'admin@gajigo.com', '', 'admin');
+VALUES (now(), now(), '', 'Admin', true, 'admin@gajigo.com', '{bcrypt}$2a$10$bJltVLK.iz6unVBxFoaWZ.1s7zwzyx3rZu61idDpDU7MXHfk77XlK', 'admin');
