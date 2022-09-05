a4099fbf59ead2d56c77021e6b3834bbf1fcc52f
changzer <changzer@gmail.com>
2022-09-05 18:22:04 -0300

    @WithMockUser(username = "admin", roles = "ADMIN") adicionado

--- src/test/java/br/com/uniamerica/gajigo/integration/UserTest2.java
+++ src/test/java/br/com/uniamerica/gajigo/integration/UserTest2.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,7 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
+@WithMockUser(username = "admin", roles = "ADMIN")
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = UserRepository.class)
 public class UserTest2 {
