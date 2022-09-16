459daf6cfe22eb3a6c7137a4479152f3ecd97ad9
Helcio L. do Carmo Junior <helcio@carmo.in>
2022-09-16 00:57:40 -0300

    chore: Rename SwaggerConfig to SwaggerConfigurer

--- src/main/java/br/com/uniamerica/gajigo/config/SwaggerConfigurer.java
+++ src/main/java/br/com/uniamerica/gajigo/config/SwaggerConfigurer.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPI30
-public class SwaggerConfig {
+public class SwaggerConfigurer {
 
     @Bean
     public GroupedOpenApi publicApi() {
