793509c5f27714b6bb92493267f8e1bcbb93816d
edua2127 <magalhaeseduardo91@gmail.com>
2022-11-25 20:26:46 -0300

    fix: resolve problema de cors allowedOrigins

--- src/main/java/br/com/uniamerica/gajigo/GajigoApplication.java
+++ src/main/java/br/com/uniamerica/gajigo/GajigoApplication.java
@@ -4,9 +4,12 @@ import br.com.uniamerica.gajigo.utils.ProjectionGenerator;
 import br.com.uniamerica.gajigo.validator.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.validation.Validator;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GajigoApplication implements RepositoryRestConfigurer {
@@ -35,5 +38,15 @@ public class GajigoApplication implements RepositoryRestConfigurer {
         validatingListener.addValidator("beforeCreate", validator);
         validatingListener.addValidator("beforeSave", validator);
         validatingListener.addValidator("beforeLinkSave", validator);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000/");
+            }
+        };
     }
 }
